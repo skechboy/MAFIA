c53f1283a1ff99927a03039d793bf61452b4729c
Trajche Petrov <skechboy@gmail.com>
2010-12-13 17:35:44 +0100

    First Commit in English language

---
+++ english.php
@@ -0,0 +1,21 @@
+<?php
+//defined( '_VALID_INC' ) or die( 'Забранет достап!!!' );
+
+/*======================================= CRIMES ==========================================*/
+DEFINE("_CRIME_INFO",					"By committing crimes you can earn lots of money, but keep in mind, the police is on a mission to catch you! The percentages with the crimes indicate your skill in a chance on success, the more crimes you commit, the better you'll get at it.");
+DEFINE("_CRIMES",						"Crimes");
+DEFINE("_SMALL_CRIMES",					"Small crimes");
+DEFINE("_LARGE_CRIMES",					"Big crimes");
+DEFINE("_REAL_LARGE_CRIMES",			"Real big crimes");
+DEFINE("_CHANSE_OF_SUCESS",				"chances of success");
+DEFINE("_MAX_REWARD",					"is the maximum reward");
+DEFINE("_JAIL_TIME",					"jail time if you are caught");
+DEFINE("_MAX_DAMAGE",					"maximum damage");
+DEFINE("_MINUTE",						"Minute");
+DEFINE("_STEAL_CANDY",					"Taking candy from a baby");
+DEFINE("_STEAL_BIKE",					"Stealing a bicycle");
+DEFINE("_PICK_POCKET",					"Pickpocketing in the city");
+DEFINE("_BREAK_CAR",					"Breaking into a car");
+DEFINE("_STEAL_FROM_LOCALSTORE",		"Stealing goods from the local department store");
+/*=================================END OF CRIMES ==========================================*/
+?>
