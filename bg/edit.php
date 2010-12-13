6dc613a394aeb581ab57a6daa313340fa132cdac
Trajche Petrov <skechboy@gmail.com>
2010-12-13 17:25:40 +0100

    First commit in bulgarian language

---
+++ bg/edit.php
@@ -0,0 +1,30 @@
+<?php
+/*================================= MY PROFILE ==========================================*/
+DEFINE("_EDIT_TITLE",    			            "Mоят профил");
+DEFINE("_EDIT_TAB1",    			            "Лична информация");
+DEFINE("_EDIT_TAB2",    			            "Лично съобщение");
+DEFINE("_EDIT_TAB3",    			            "Коментари");
+DEFINE("_EDIT_TAB4",    			            "Промяна на парола");
+DEFINE("_EDIT_CHECK1",    			            "Не желая да получавам информация за новостите в играта");
+DEFINE("_EDIT_BTN1",    			            "Промени");
+DEFINE("_EDIT_BTN2",    			            "Искам да си сменя паролата");
+DEFINE("_EDIT_BTN3",   			                "Убийци");
+DEFINE("_SEX",  			                    "Пол");
+DEFINE("_MAN",    			           			"Мъж");
+DEFINE("_WOMAN",    			            	"Жена");
+DEFINE("_EDIT_ERROR1",    			            "Не си попълнил всички полета!");
+DEFINE("_EDIT_ERROR2",    			            "Написаната информация не е коректна!");
+DEFINE("_EDIT_MSG1",    			            "Паролата беше сменена успешно!");
+DEFINE("_EDIT_MSG2",    			            "Информацията беше променена успешно!");
+DEFINE("_EDIT_INFO1",    			            "Забележка: Можеш да вмъкваш снимки и линкове чрез [url] и [img]");
+DEFINE("_EDIT_INFO2",    			            "From here you can again a confirmation email to send to your inbox. Just be sure, you can email it send it 5 times after yourself. You may have this email 3 times invested hours.");
+
+DEFINE("_EDIT_NOCOMMENTS",    			        "В момента няма коментари");
+DEFINE("_EDIT_CELL1",    			            "Стара парола");
+DEFINE("_EDIT_CELL2",    			            "Нова парола");
+DEFINE("_EDIT_CELL3",    			            "Нова парола");
+
+
+DEFINE("_EDIT_BTN1",    			            "Промени");
+DEFINE("_EDIT_BTN2",    			            "Искам да си сменя паролата");
+?>
---
+++ bg/message.php
@@ -0,0 +1,30 @@
+<?php
+/*=================================  MESSAGES  ==========================================*/
+DEFINE("_MSG_TITLE",					"Съобщения");
+DEFINE("_MSG_INBOX",					"Получени");
+DEFINE("_MSG_OUTBOX",					"Изпратени");
+DEFINE("_MSG_SYSTEM",					"Системни");
+DEFINE("_MSG_NEW",	    				"Напиши");
+DEFINE("_MSG_DELALL",					"Изчисти");
+DEFINE("_MSG_MSG1",	 				    "Съобщението е изпратено");
+DEFINE("_MSG_MSG2",    			        "те е добавил в черният списък и неможеш да му пишеш!");
+DEFINE("_MSG_MSG3",					    "Съобщението <strong>НЕ</strong> е изпратено!");
+DEFINE("_MSG_BLACKLIST",	    		"Добави в черният списък");
+DEFINE("_TO",           			    "До");
+DEFINE("_FROM",			    	    	"От");
+DEFINE("_REPLY",					    "Отговори");
+DEFINE("_MSG_FRIENDS",				    "||| ПРИЯТЕЛИ |||");
+DEFINE("_MSG_ADD",						"Добави като контакт");
+DEFINE("_SEND",				            "Изпрати");
+DEFINE("_MSG_ERROR1",					"Моля въведете заглавие");
+DEFINE("_MSG_ERROR2",					"Моля въведете получател");
+DEFINE("_MSG_ERROR3",					"Моля въведете съобщение");
+DEFINE("_NEWS_INBOX_ALL",				"Всички съобщения");
+DEFINE("_NEWS_INBOX_EMPTY",				"Нямате получени съобщения");
+DEFINE("_NEWS_FORUM_ALL",				"Виж форума");
+DEFINE("_NOT_READED",					"Пораката не ви е прочитана");
+DEFINE("_READED",						"Пораката ви е прочитана");
+DEFINE("_DELETE",						"Избриши");
+DEFINE("_QUOTE",						"Цитат");
+
+?>
