220b0f5854b4d84bcb7f85d988f667609ef7fbe8
Trajche Petrov <skechboy@gmail.com>
2010-12-24 15:08:28 +0100

    Added weapons and user info language files

---
+++ bg/famprofile.php
@@ -0,0 +1,30 @@
+<?php
+/*================================= FAMILY PROFILE ==========================================*/
+DEFINE("_FP_FOUNDER",				   	"Основател");
+DEFINE("_FP_MEMBERS",				    "Членове");
+DEFINE("_MAX",							"Максимален брой ");
+DEFINE("_FP_LEAVE",						"Напусни фамилията");
+DEFINE("_FP_DONATION",					"Дарение");
+DEFINE("_FP_NOTHING",				   	"Фамилията няма никаква собственост");
+DEFINE("_FP_RANG",				   		"Ранг в фамилията");
+DEFINE("_FP_MOTO",						"Мото");
+
+DEFINE("_F_ITEM1",				   		"Хотели");
+DEFINE("_F_ITEM2",				   		"Мощ за фамилията");
+DEFINE("_F_ITEM3",				   		"Бойни кораби");
+DEFINE("_F_ITEM4",				   		"Самолетоносачи");
+DEFINE("_F_ITEM5",				   		"Бомби 'Daisy cutter'");
+DEFINE("_F_ITEM6",				   		"Минохвъргачки");
+DEFINE("_F_ITEM7",				   		"Танкове");
+DEFINE("_F_ITEM8",				   		"Хеликоптери");
+DEFINE("_F_ITEM9",				   		"Воени ловци");
+DEFINE("_P_KILLERS",   			            "Убийци");
+DEFINE("_P_WINS",    			            "Брой победи");
+DEFINE("_P_LOSES",    			            "Брой загуби");
+DEFINE("_P_BTN3",    			            "Таен линк");
+DEFINE("_P_INFO2",    			            "Собственост");
+DEFINE("_P_BTN1",    			            "Нападни");
+DEFINE("_SHOP_ITEM14",    			        "Поени за напад");
+DEFINE("_RACE_COL3",    			        "Стойност");
+DEFINE("_NO_FAMILIES",    			    "Сеуште нема оформени фамилии...");
+?>
---
+++ bg/userinfo.php
@@ -0,0 +1,11 @@
+<?php 
+DEFINE("_CASH",					" Кеш");
+DEFINE("_BANK",					" Финанси");
+DEFINE("_BANKUP",			    " Банка");
+DEFINE("_POWER",			 	" Мощ");
+DEFINE("_HEALTH",				" Здраве");
+DEFINE("_RANG",                 " Ранг");
+DEFINE("_CITY",	        		" Град");
+DEFINE("_HIDDEN",	            " Скрит");
+DEFINE("_MONEYPREFIX",			"&nbsp;€&nbsp;");
+?>
