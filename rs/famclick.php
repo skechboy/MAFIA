56b4c3bfdb39c96a873509b424868d0b88fb3df7
Trajche Petrov <skechboy@gmail.com>
2010-12-24 15:13:13 +0100

    Removed other language files

--- rs/famclick.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-DEFINE("_FCL_ITEM1",			   		" Još ima ");
-DEFINE("_FCL_ITEM2",			   		" Obučene ubice!");
-DEFINE("_FCL_ITEM3",			   		"Već ste plaćeni ubica. sutra, pokušajte ponovo!");
-?>
--- rs/userinfo.php
+++
@@ -1,9 +0,0 @@
-﻿<?php 
-DEFINE("_CASH",					" Keš");
-DEFINE("_BANK",					" Finansije");
-DEFINE("_BANKUP",			    	" Banka");
-DEFINE("_POWER",			 	" Moć");
-DEFINE("_HEALTH",				" Zdravlje");
-DEFINE("_RANG",                        		" Rang");
-DEFINE("_CITY",	        		        " Grad");
-?>
